c8bbabf4b89dc6c5b061058cab6d33e31da3024a
ElijahMac <kilobolt1@mail.com>
2022-04-12 05:01:44 -0500

    configed jest/babel again...

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     transform: {
         /* Use babel-jest to transpile tests with the next/babel preset
         https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-        '^.+\\.(js)$': ['babel-jest', { presets: ['next/babel'] }],
+        '^.+\\.(js)$': ['babel-jest'],
     },
     setupFilesAfterEnv: ['<rootDir>/__tests__/jest.setup.js']
 }
